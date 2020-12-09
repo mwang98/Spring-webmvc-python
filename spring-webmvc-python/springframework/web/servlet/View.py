ff78d12c8bf8bbc0fb849a72dc4d695ecb33680f
nick1889 <r07922151@ntu.edu.tw>
2020-12-10 01:43:17 +0800

    edit View

--- spring-webmvc-python/springframework/web/servlet/View.py
+++ spring-webmvc-python/springframework/web/servlet/View.py
@@ -1,12 +1,12 @@
-class View():
+class View:
     
     def __init__(self):
         self.RESPONSE_STATUS_ATTRIBUTE = self.__class__.__name__ + ".responseStatus"
         self.PATH_VARIABLES = self.__class__.__name__ + ".pathVariables"
         self.SELECTED_CONTENT_TYPE = self.__class__.__name__ + ".selectedContentType"
-        
-    def getContentType(self):
+
+    def get_content_type(self):
         return None
-    
+
     def render(self, request, response, model=None):
         raise NotImplementedError
