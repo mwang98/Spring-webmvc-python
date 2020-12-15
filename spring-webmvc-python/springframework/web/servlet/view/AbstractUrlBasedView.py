dc65df7d195bfb13077b3b0e195a3da24152e332
Jason Tsai <jason.tsai@appier.com>
2020-12-15 15:59:35 +0800

    add AbstractUrlView

--- spring-webmvc-python/springframework/web/servlet/view/AbstractUrlBasedView.py
+++ spring-webmvc-python/springframework/web/servlet/view/AbstractUrlBasedView.py
@@ -1,1 +1,29 @@
-class AbstractUrlBasedView(metaclass=AbstractView)
+from springframework.web.servlet.view import AbstractView
+
+
+class AbstractUrlBasedView(metaclass=AbstractView):
+
+    url: str = None
+
+    def __init__(self, url: str):
+        self.url = url
+
+    def setUrl(self, url: str) -> None:
+        self.url = url
+
+    def getUrl(self) -> str:
+        return self.url
+
+    def afterPropertiesSet(self) -> None:
+        if self.isUrlRequired() and self.getUrl() is None:
+            raise ValueError("Property 'url' is required")
+
+    def isUrlRequired(self) -> bool:
+        return True
+
+    def checkResource(self, locale) -> bool:
+        # local type: Locale
+        return True
+
+    def toString(self) -> str:
+        return super().toString() + f"; URL [{self.getUrl()}]"
