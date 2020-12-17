53474123cb1888d7be04a789dde77ffca6e303df
Jason Tsai <jason.tsai@appier.com>
2020-12-17 19:23:31 +0800

    add mock http session

---
+++ spring-webmvc-python/springframework/utils/mock.py
@@ -0,0 +1,13 @@
+from unittest import mock
+
+HttpHeaders = type("HttpSessionBindingEvent", (mock.MagicMock, ), {})
+ServletInputStream = type("ServletInputStream", (mock.MagicMock, ), {})
+DelegatingServletInputStream = type("DelegatingServletInputStream", (mock.MagicMock, ), {})
+BufferedReader = type("BufferedReader", (mock.MagicMock, ), {})
+ByteArrayInputStream = type("ByteArrayInputStream", (mock.MagicMock, ), {})
+InputStreamReader = type("InputStreamReader", (mock.MagicMock, ), {})
+MediaType = type("MediaType", (mock.MagicMock, ), {})
+Locale = type("Locale", (mock.MagicMock, ), {})
+HttpSessionBindingEvent = type("HttpSessionBindingEvent", (mock.MagicMock, ), {})
+HttpSessionBindingListener = type("HttpSessionBindingListener", (mock.MagicMock,), {})
+Serializable = type("Serializable", (mock.MagicMock,), {})
