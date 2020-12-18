b50e809ed297b401251b9d2ab11d8d6b6d55d420
nick1889 <r07922151@ntu.edu.tw>
2020-12-18 21:36:36 +0800

    add some dependencies of View

--- spring-webmvc-python/springframework/context/MessageSource.py
+++ spring-webmvc-python/springframework/context/MessageSource.py
@@ -1,11 +1,11 @@
-from abc import ABC, ABCMeta, abstractmethod
-from springframework.function.support.multiple import MultipleMeta
+from abc import ABC, abstractmethod
 from typing import List
 
+from mock.inst import Locale
 
-class MessageSource(ABC, MultipleMeta):
+
+class MessageSource(ABC):
     @abstractmethod
-    def get_message(self, code: str, args: List[object], default_message: str, locale: Locale):
+    def get_message(self, locale: Locale, default_message: str = None, resolvable: MessageSourceResolvable = None,
+                    code: str = None, args: List[object] = None):
         raise NotImplementedError
-
-    # TODO: function overloading issue
