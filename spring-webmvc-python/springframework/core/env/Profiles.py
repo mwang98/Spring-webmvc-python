7a8492cabce6d1af3a62ab4b588f1ece474a3eb1
nick1889 <r07922151@ntu.edu.tw>
2020-12-30 03:44:44 +0800

    add test view

--- spring-webmvc-python/springframework/core/env/Profiles.py
+++ spring-webmvc-python/springframework/core/env/Profiles.py
@@ -1,2 +1,13 @@
-class Profiles():
-    pass
+from abc import ABC, abstractmethod
+
+from env.ProfilesParser import ProfilesParser
+
+
+class Profiles(ABC):
+    @abstractmethod
+    def matches(self, active_profiles) -> bool:
+        raise NotImplementedError
+
+    @classmethod
+    def of(cls, *profiles):
+        return ProfilesParser.parse(profiles)
