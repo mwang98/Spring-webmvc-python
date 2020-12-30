bda531d470267df256de2f142d4771cac5a054a8
Jason Tsai <jason.tsai@appier.com>
2020-12-30 15:30:05 +0800

    gitignore & internalresourceviewtest

--- spring-webmvc-python/tests/test_InternalResourceView.py
+++ spring-webmvc-python/tests/test_InternalResourceView.py
@@ -1,16 +1,20 @@
 import pytest
 from unittest import TestCase
+from springframework.web.servlet import View
 from springframework.web.testfixture.servlet import MockHttpServletRequest as HttpServletRequest
+from springframework.web.testfixture.servlet import MockHttpServletResponse as HttpServletResponse
+from springframework.web.testfixture.servlet import MockServletContext, MockRequestDispatcher
 from springframework.web.servlet.view import InternalResourceView
 
 
 @pytest.mark.skip("half way to complete")
 class TestInternalResourceView(TestCase):
-    _model = {'foo': 'bar', 'I': 1}
-    url = 'forward-to'
 
     def setUp(self) -> None:
+        self.model = {'foo': 'bar', 'I': 1}
+        self.url = 'forward-to'
         self.request = HttpServletRequest()
+        self.response = HttpServletResponse()
         self.view = InternalResourceView()
 
     def test_set_always_include(self):
@@ -25,3 +29,34 @@ class TestInternalResourceView(TestCase):
     def test_use_include(self):
         self.skipTest("TODO")
         # self.fail()
+
+    def test_forward(self):
+        request = HttpServletRequest("GET", "/myservlet/handler.do")
+        request.set_context_path("/mycontext")
+        request.set_servlet_path("/myservlet")
+        request.set_path_info(";mypathinfo")
+        request.set_query_string("?param1=value1")
+
+        self.view.set_url(self.url)
+        tmp = MockServletContext()
+        tmp.minorVersion = 4
+        self.view.set_servlet_context(tmp)
+
+        self.view.render(self.model, request, self.response)
+        self.assertEqual(self.response.get_forwarded_url(), self.url)
+        for key, value in self.model.items():
+            msg = f"Values for model key '{key}' must match"
+            self.assertEqual(request.get_attribute(key), value, msg=msg)
+
+    def test_always_include(self):
+        self.assertIsNone(self.request.getAttribute(View.PATH_VARIABLES))
+        # self.assertEqual(self.request.get_request_dispatcher(self.url)), MockRequestDispatcher(self.url))
+
+        self.view.set_url(self.url)
+        self.view.set_always_include(True)
+
+        # Can now try multiple tests
+        self.view.render(self.model, self.request, self.response)
+        self.assertEqual(self.response.get_included_url(), self.url)
+        for key, value in self.model:
+            self.request.set_attribute(key, value)
