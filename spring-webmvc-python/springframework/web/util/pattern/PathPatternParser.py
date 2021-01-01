6bcf23e96e1183062e5c2889a5be682fe41b5d93
alechuang98 <alechuang98@gmail.com>
2021-01-01 15:54:58 +0800

    Add mock class for HandlerMapping

---
+++ spring-webmvc-python/springframework/web/util/pattern/PathPatternParser.py
@@ -0,0 +1,12 @@
+from springframework.web.util.pattern import PathPattern
+
+
+class PatternParser():
+    
+    def __init__(self):
+        return
+    
+    def parse(self) -> PathPattern:
+        return None
+
+    
