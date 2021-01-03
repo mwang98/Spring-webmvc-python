850bf8fb7d8c10be19443f2b6990171366fbff8c
Tony <tonylin1223@gmail.com>
2021-01-03 16:16:22 +0800

    fix

--- spring-webmvc-python/tests/web/servlet/test_DispatcherServlet.py
+++ spring-webmvc-python/tests/web/servlet/test_DispatcherServlet.py
@@ -2,15 +2,21 @@
 from springframework.web.servlet.DispatcherServlet import DispatcherServlet
 from springframework.web.testfixture.servlet.MockServletConfig import MockServletConfig
 from springframework.web.testfixture.servlet.MockServletContext import MockServletContext
-
+from springframework.web.testfixture.servlet.MockHttpServletRequest import MockHttpServletRequest
+from springframework.web.testfixture.servlet.MockHttpServletResponse import MockHttpServletResponse
 
 def main():
     print('strat')
     servletConfig = MockServletConfig(MockServletContext, servletName='simple')
     dispatcherServelet = DispatcherServlet()
+    dispatcherServelet.init(servletConfig)
+
+    request = MockHttpServletRequest(servletConfig.get_servlet_context(), "GET", "/locale.do")
+    #request.addPreferredLocale(Locale.CANADA)
+    response = MockHttpServletResponse()
 
     #dispatcherServelet.set_context_class()
-    dispatcherServelet.init(servletConfig)
+    dispatcherServelet.do_service(request, response)
     print('test finished')
 
 
